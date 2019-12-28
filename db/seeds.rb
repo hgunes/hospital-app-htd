caf88cb0f1082153eaf4397f1059b29f6ab83fa9
Harun Gunes <h.gunes13@gmail.com>
2019-12-28 23:00:06 +0100

    Updated seeds file to create 20 doctors and 20 patients for insights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
-20.times { FactoryBot.create(:patient) }
-20.times { FactoryBot.create(:doctor) }
+20.times do |doctor|
+  Doctor.create!(
+    first_name: 'Doctor',
+    last_name: "#{doctor}",
+    practicing_from: Time.now,
+    qualification_number: doctor,
+    employment: 0
+  )
+end
+
+20.times do |patient|
+  Patient.create!(
+    first_name: "Patient",
+    last_name: "#{patient}",
+    age: patient + 20,
+    gender: 1
+  )
+end
